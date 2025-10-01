ab74a69f6b860a0c6747507008981c059fcec1db
Tracer-asharmaz <lasha.sharmazanashvili@gmail.com>
2025-10-02 00:10:13 +0400

    Initial commit

---
+++ Assets/Scenes/VRCDefaultWorldScene_UdonProgramSources/FloorCollider.cs
@@ -0,0 +1,43 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace FloorCollider
+{ 
+public class FloorCollider : UdonSharpBehaviour
+{
+    public GameObject[] StairsObjects;
+    public int[] Stairs;
+
+    private void Start()
+    {
+        foreach (var stair in StairsObjects)
+        {
+            stair.SetActive(true);
+        }
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (player.IsValid() && player.isLocal)
+        {
+            foreach (var stair in StairsObjects)
+            {
+                stair.SetActive(false);
+            }
+        }
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (!player.IsValid() || !player.isLocal) return;
+
+        foreach (var stair in StairsObjects)
+        {
+            stair.SetActive(true);
+        }
+    }
+}
+}
